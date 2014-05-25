0bb2706f120afb7f51057f8d64f675fd8af1332e
Guilherme Guerra <guerrinha@comum.org>
2014-05-25 15:02:32 -0300

    Exibe os posts da home
      - Posts da categoria destaques são exibidos na home
      - Formatação dos posts na home

--- index.php
+++ index.php
@@ -18,11 +18,11 @@
 	}
 	$context = Timber::get_context();
 	$context['posts'] = Timber::get_posts();
-	$context['foo'] = 'bar';
+        $args = 'category_name=destaques';
+	$context['destaques'] = Timber::get_posts($args);
+
 	$templates = array('index.twig');
 	if (is_home()){
 		array_unshift($templates, 'home.twig');
 	}
 	Timber::render($templates, $context);
-
-
