bf04734ec85edf198a8e349fa06bea6572a72cc3
Guilherme Guerra <guerrinha@comum.org>
2014-05-25 12:58:32 -0300

    Define a fonte museo como padrão

--- config.rb
+++ config.rb
@@ -7,6 +7,7 @@ css_dir = "."
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
+font_dir = "../fonts"
 
 output_style = :expanded
 environment = :development
@@ -15,7 +16,7 @@ environment = :development
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
