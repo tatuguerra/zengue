9a7b4a56c9d0408a985f3d50cb80fd0648080e98
Guilherme Guerra <guerrinha@comum.org>
2014-06-04 23:32:33 -0300

    Adiciona suporte para widgets na página de visualização da notícia

--- functions.php
+++ functions.php
@@ -32,3 +32,16 @@
 	function load_scripts(){
 		wp_enqueue_script('jquery');
 	}
+
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name' => 'Sidebar',
+		'id' => 'sidebar',
+		'before_widget' => '<div class="widget">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4>',
+		'after_title' => '</h4>',
+	) );
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
--- single.php
+++ single.php
@@ -14,5 +14,6 @@ $post = new TimberPost();
 $context['post'] = $post;
 $context['wp_title'] .= ' - ' . $post->title();
 $context['comment_form'] = TimberHelper::get_comment_form();
+$context['widgets'] = Timber::get_widgets('sidebar');
 
 Timber::render(array('single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig'), $context);
