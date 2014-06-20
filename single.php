54d82391415df46c96958ac700c033e7b2d5f981
Guilherme Guerra <guerrinha@comum.org>
2014-06-20 17:04:59 -0300

    Adiciona post type colunistas

--- single.php
+++ single.php
@@ -15,5 +15,6 @@ $context['post'] = $post;
 $context['wp_title'] .= ' - ' . $post->title();
 $context['comment_form'] = TimberHelper::get_comment_form();
 $context['widgets'] = Timber::get_widgets('sidebar');
-
+$blog_args = 'post_type=colunistas';
+$context['colunistas'] = Timber::get_posts($blog_args);
 Timber::render(array('single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig'), $context);
