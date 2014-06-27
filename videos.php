b6aba5a16baf96bfe5d227ed16892e0221ad4dff
Guilherme Guerra <guerrinha@comum.org>
2014-06-27 11:07:18 -0300

    Melhora a disposição dos vídeos

--- videos.php
+++ videos.php
@@ -1,6 +1,8 @@
 <?php
 
 $context = Timber::get_context();
+$blog_args = 'post_type=colunistas';
+$context['colunistas'] = Timber::get_posts($blog_args);
 $movie_args = 'post_type=videos';
 $context['videos'] = Timber::get_posts($movie_args);
 
